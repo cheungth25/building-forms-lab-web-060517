82882f008d7a0364f3740482515bf6dea5f52e82
cheungth25 <cheungth25@gmail.com>
2017-08-28 14:39:48 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,7 +3,8 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return Object.assign({}, state, { bands: state.bands.concat(action.band) });
+    console.log("in manageBands",{bands: [...state.bands, action.band]})
+      return {bands: [...state.bands, action.band]}
     default:
       return state;
   }
